402a71accfb1ab55d17df5aea44de76042e392ac
Arkadiusz Ziobrowski <arkadiusz@bluelabs.eu>
2023-12-08 17:24:48 +0100

    feat: day 8, part 2

--- day8/day8.pl
+++ day8/day8.pl
@@ -18,17 +18,40 @@ inside_parenthesis(N, L, R) -->
   { string_codes(N, Ns), string_codes(L, Ls), string_codes(R, Rs) }.
 b --> blanks.
 
+
+navigate(NavigationList, Index, Direction) :-
+    length(NavigationList, Len),
+    I is Index mod Len,
+    nth0(I, NavigationList, Direction).
+
 % running part 1:
 % ?- read_file_to_facts('input.txt', Navigation), string_chars(Navigation, NavigationList), part1(NavigationList, "AAA", 0, Out).
 
-part1(_, "ZZZ", Index, Out) :- Out is Index, !.
+% uncomment for part 1.
+% part1(_, "ZZZ", Index, Out) :- Out is Index, !.
+
+% uncomment for part 2.
+part1(_, Node, Index, Out) :- ends_with(Node, 'Z'), Out is Index, !.
 part1(NavigationList, StartNode, NavigationIndex, Out) :-
     node(StartNode, (L, R)),
     navigate(NavigationList, NavigationIndex, Direction),
     NewIndex is NavigationIndex + 1,
     (Direction = 'L' -> part1(NavigationList, L, NewIndex, Out) ; part1(NavigationList, R, NewIndex, Out)).
 
-navigate(NavigationList, Index, Direction) :-
-    length(NavigationList, Len),
-    I is Index mod Len,
-    nth0(I, NavigationList, Direction).
+% running part 2:
+% ?- read_file_to_facts('input.txt', Navigation), string_chars(Navigation, NavigationList), part2(NavigationList, Out).
+part2(NavigationList, Out) :-
+    findall(StartNode, (node(StartNode, _), ends_with(StartNode, 'A')), NodesEndingWithA),
+    findall(PathLength, (member(Node, NodesEndingWithA), part1(NavigationList, Node, 0, PathLength)), PathLengths),
+    lcm_list(PathLengths, Out).
+
+
+ends_with(String, Char) :-
+    atom_codes(String, StringCodes),
+    atom_codes(Char, [CharCode]),
+    append(_, [CharCode], StringCodes).
+
+lcm_list([X], X).
+lcm_list([X | Xs], LCM) :-
+    lcm_list(Xs, TempLCM),
+    LCM is lcm(X, TempLCM).
